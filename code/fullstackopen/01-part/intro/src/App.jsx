73122fb85c55d00cadc525675a47dd44fba32ffc
sayanta <sayantasingh01@gmail.com>
2025-03-13 03:52:32 +0530

    update

---
+++ code/fullstackopen/01-part/intro/src/App.jsx
@@ -0,0 +1,48 @@
+function Header() {
+  const course = "Half Stack App Dev";
+  return <h1>{course}</h1>;
+}
+
+function Content() {
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+
+      <Total exercises={exercises1 + exercises2 + exercises3} />
+      {/* <Total exercises={{ exercises1, exercises2, exercises3 }} /> */}
+    </>
+  );
+}
+
+function Total({ exercises }) {
+  return (
+    <>
+      <p>Number of exercises {exercises}</p>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+}
+
+export default App;
